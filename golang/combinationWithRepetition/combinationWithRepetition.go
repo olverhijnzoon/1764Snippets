c0bff577e96ff1f2c4b6180d3cc9175413edc81f
olverhijnzoon <olver.hijnzoon@gmail.com>
2023-12-31 12:35:07 +0100

    Golang Combinations with Repetition

---
+++ golang/combinationWithRepetition/combinationWithRepetition.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
+func combination(n, r int) int {
+	return factorial(n) / (factorial(r) * factorial(n-r))
+}
+
+func permutation(n, r int) int {
+	return factorial(n) / factorial(n-r)
+}
+
+func combinationWithRepetition(n, r int) int {
+	return factorial(n+r-1) / (factorial(r) * factorial(n-1))
+}
+
+func main() {
+	fmt.Println("# 1764Snippets")
+	fmt.Println("## Golang Combination with Repetition")
+
+	fmt.Println("Factorial of 5 is", factorial(5))
+	fmt.Println("Combination of 5 and 3 is", combination(5, 3))
+	fmt.Println("Permutation of 5 and 3 is", permutation(5, 3))
+	fmt.Println("Combination with repetition of 5 and 3 is", combinationWithRepetition(5, 3))
+}
---
+++ golang/combinationWithRepetition/go.mod
@@ -0,0 +1,3 @@
+module github.com/olverhijnzoon/1764Snippets/combinationWithRepetition
+
+go 1.21
