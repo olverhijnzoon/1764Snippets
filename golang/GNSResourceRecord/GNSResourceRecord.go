404fca053954e681d9908097edc1884592127a4b
Oliver Heinsohn <119976020+olverhijnzoon@users.noreply.github.com>
2024-01-01 16:20:55 +0100

    GNS Resource Record Types (#12)
    * GNS Resource Record Types
    
    * GNS Resource Record Types
    
    * GNS Resource Record Types

--- golang/GNSResourceRecord/GNSResourceRecord.go
+++ golang/GNSResourceRecord/GNSResourceRecord.go
@@ -44,9 +44,13 @@ func main() {
 
 	gns := NewGNS()
 
-	gns.AddRecord("example", "127.0.0.1", "A", 3600)
+	// Add a record of type "PKEY" GNS zone delegation
+	gns.AddRecord("publickey", "30820122300d06092a864886f70d01010105000382010f003082010a0282010100b11e30f8b4c5ba6d7f90f79e4e38c3b2cb8b2c9f2a36c63a6b8c5b5a9292487a0f98a0d69f9e39a3726e8268e0a0b3f8b5c034b1331a3f3a6c1a3a1402065574a2f2e4e7a0a0a2b6e8a8a8a2a8a0a8a2a8a8a8a2a8a0a8a2a8a8a8a2a8a0a8a2a8a8", "PKEY", 3600)
 
-	record, ok := gns.GetRecord("example")
+	// Add a record of type "NICK" GNS zone nickname
+	gns.AddRecord("nickname", "1764SnippetsZone", "NICK", 3600)
+
+	record, ok := gns.GetRecord("publickey")
 	if ok {
 		fmt.Printf("Record: %v\n", record)
 	} else {
