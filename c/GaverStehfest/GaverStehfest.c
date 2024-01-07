2755f1dc254f054ce58150b2efb337cad6f46de2
olverhijnzoon <olver.hijnzoon@gmail.com>
2024-01-07 22:01:56 +0100

    Some attempts to fix the code

--- c/GaverStehfest/GaverStehfest.c
+++ c/GaverStehfest/GaverStehfest.c
@@ -5,14 +5,21 @@
 
 double v[N];
 
+unsigned long factorial(int n) {
+    if (n == 0)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
 // Calculate the coefficients for the Gaver-Stehfest algorithm
 void calculateCoefficients() {
     int halfCoefficientCount = N / 2;
     v[0] = 0.000001;
-    for (int n = 1; n < N; n++) {
+    for (int n = 1; n <= N; n++) {
         double z = 0.0;
         for (int k = (n + 1) / 2; k <= fmin(n, halfCoefficientCount); k++) {
-            z += pow(k, halfCoefficientCount) * tgamma(2 * k) / (tgamma(halfCoefficientCount - k) * tgamma(k) * tgamma(k) * tgamma(n - k) * tgamma(2 * k - n));
+            z += pow(k, halfCoefficientCount) * factorial(2 * k) / (factorial(halfCoefficientCount - k) * factorial(k) * factorial(k - 1) * factorial(n - k) * factorial(2 * k - n));
         }
         v[n] = pow(-1, n + halfCoefficientCount) * z;
         printf("z: %f\n", z);
@@ -24,6 +31,7 @@ double F(double s) {
     // constant in the exponential function e^at
     // double a = 1.0;
     // return 1.0 / (s + a);
+
     // t^2
     return 2.0 / (s*s*s);
 }
@@ -32,18 +40,18 @@ double F(double s) {
 double inverseLaplaceTransform(double t) {
     double sum = 0.0;
     double ln2_t = log(2.0) / t;
-    for (int n = 0; n < N; n++) {
+    for (int n = 0; n <= N; n++) {
         double p = (n + 1) * ln2_t;
         sum += v[n] * F(p);
     }
-    // TODO: It somehow works for small t like this but instead of "/ 176697", it would be better to really find out what is wrong. Maybe v[N-2] and v[N-1] coefficients? Or tgamma(k)? <=?
-    return sum * ln2_t / 176697;
+    // TODO: Code remains with issues - see diff to last commit; "/ 18361788250959268" is more like a joke.  
+    return sum * ln2_t / 18361788250959268;
 }
 
 int main() {
     calculateCoefficients();
 
-    double t = 2;
+    double t = 6;
     printf("Inverse Laplace Transform at t=%f: %f\n", t, inverseLaplaceTransform(t));
 
     return 0;
