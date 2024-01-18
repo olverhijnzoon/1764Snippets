1ee61ca2cfdeae65a6d20db4f430901e0053864f
olverhijnzoon <olver.hijnzoon@gmail.com>
2024-01-18 19:57:12 +0100

    Ignition addTags Draft

---
+++ ignition/tags/addTags.sql
@@ -0,0 +1,2 @@
+INSERT INTO TAGCONFIG (ID, PROVIDERID, FOLDERID, CFG, RANK, NAME)
+VALUES ('623417bd-b01a-44ef-a0b7-504e87e0c1cd', 0, '', '{\n\"valueSource\": \"derived\",\n\"name\": \"Snippets\",\n\"tagType\": \"AtomicTag\"\n}', 1, 'Snippets');
