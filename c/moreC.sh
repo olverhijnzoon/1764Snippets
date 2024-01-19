9e8304060f2550a0ea30a8d63278edcbb35dba0d
olverhijnzoon <olver.hijnzoon@gmail.com>
2024-01-19 15:40:24 +0100

    Cleanup moreC script

--- c/moreC.sh
+++ c/moreC.sh
@@ -16,24 +16,8 @@ cat <<EOF >$snippetname.c
 int main() {
     
     printf("1764Snippets\n");
-    printf("SymRank2 MoreC\n");
-
-    int matrix[4][4] = {
-        {1, 2, 3, 4},
-        {2, 5, 6, 7},
-        {3, 6, 8, 9},
-        {4, 7, 9, 10}
-    };
-
-    printf("4x4 Symmetric Matrix:\\n");
-    for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++) {
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\\n");
-    }
-
-    return 0;
+    printf("MoreC\n");
+    
 }
 EOF
 
