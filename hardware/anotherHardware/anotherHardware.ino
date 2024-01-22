fb9db7a51baca564cb68d2182f369eb5f5e0de03
olverhijnzoon <olver.hijnzoon@gmail.com>
2024-01-22 14:29:22 +0100

    Arduino UNO simple blink

---
+++ hardware/anotherHardware/anotherHardware.ino
@@ -0,0 +1,12 @@
+void setup() {
+  Serial.begin(9600);
+  delay(1000);
+
+  Serial.println("## 1764Snippets");
+  Serial.println("## Hardware Arduino UNO");
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // Your loop code here
+}
